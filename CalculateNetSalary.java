072152d715eb82c960fac07dd67d9c8eae45d5dc
FeiBaliou <48136578+FeiBaliou@users.noreply.github.com>
2020-07-05 23:24:23 +0300

    Update CalculateNetSalary.java

--- CalculateNetSalary.java
+++ CalculateNetSalary.java
@@ -4,26 +4,40 @@ taxes 9%
 Calculate the Net Annual Salary and the ammount of taxes. */
 
 
+import java.util.Scanner;
+
 public class CalculateNetSalary {
 
-   public static void main(String[] args){
+public static void main(String[] args){
 
-   ammountOfTaxes(900, 0.09f);
-   netAnnulaIncome(900,0.09f);
+
+Scanner in = new Scanner(System.in);
+
+System.out.println("Input your monthly gross salary: ");
+
+float monthlySalary = in.nextFloat();
+
+float percentageOfTaxes = 0.09f;
+
+System.out.println("Your monthly gross salary is " + monthlySalary + "$ and Government taxes you " + (percentageOfTaxes * 100) + "% .");
+
+   netAnnulaIncome(in,monthlySalary,percentageOfTaxes);
+   ammountOfTaxes(in,monthlySalary, percentageOfTaxes);
 }
 
 
 //Calculate the ammount of taxes for the year.
-public static float ammountOfTaxes(int monthlySalary, float percentageOfTaxes) {
+public static void ammountOfTaxes(Scanner in,float monthlySalary, float percentageOfTaxes) {
         float taxes = ((12*monthlySalary) * percentageOfTaxes );
-        System.out.println("The ammount of taxes for the Year is: " + taxes + "$");
-        return taxes;
+        System.out.println("The total ammount of taxes for the Year is: " + taxes + "$");
+        
   }
 
-public static float netAnnulaIncome(int monthlySalary, float percentageOfTaxes) {
+public static void netAnnulaIncome(Scanner in,float monthlySalary, float percentageOfTaxes) {
         float netIncome = ((12*monthlySalary) - (12*monthlySalary) * percentageOfTaxes );
-        System.out.println("The net Annual Salary is: " + netIncome + "$");
-        return netIncome;
+        System.out.println("Your net Annual Salary is: " + netIncome + "$");
+        
   }
 
 }
+
